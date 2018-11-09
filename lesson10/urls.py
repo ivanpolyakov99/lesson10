3d810126777565930ab23ecd043042dfa0293b9e
tima-akulich <tima.akulich@gmail.com>
2018-11-09 21:46:43 +0300

    whitenoise

--- lesson10/urls.py
+++ lesson10/urls.py
@@ -29,5 +29,4 @@ urlpatterns = i18n_patterns(
 )
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
