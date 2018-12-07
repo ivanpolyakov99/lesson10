8e984bea6d3dee3e07168b5f7d321fce4f30c6cf
tima-akulich <tima.akulich@gmail.com>
2018-12-07 21:56:14 +0300

    Celery.

--- lesson10/celery.py
+++ lesson10/celery.py
@@ -1,9 +1,18 @@
 import os
+import django
 
 from celery import Celery
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lesson10.settings')
+django.setup()
+
+
+from testing.tasks import SCHEDULE as TESTING_SCHEDULE
 app = Celery('lesson10')
 
+
+SCHEDULE = {}
+SCHEDULE.update(TESTING_SCHEDULE)
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = SCHEDULE
 app.autodiscover_tasks()
--- testing/tasks.py
+++ testing/tasks.py
@@ -1,9 +1,39 @@
 from time import sleep
 
 from celery import shared_task
+from django.core.mail import send_mail
+
+from celery.schedules import crontab
 
 
 @shared_task
 def func_sum(a, b):
     sleep(10)
     return a + b
+
+
+@shared_task
+def send_daily_test_report():
+    emails = ['tima.akulich@gmail.com']
+    send_mail(
+        subject='Test task',
+        message='test',
+        from_email='admin@google.com',
+        recipient_list=emails
+    )
+
+
+SCHEDULE = {
+    # 'send_daily_test_report': {
+    #     'task': 'testing.tasks.send_daily_test_report',
+    #     'args': (),
+    #     'options': {},
+    #     'schedule': timedelta(seconds=5)
+    # },
+    'send_daily_test_report_1': {
+        'task': 'testing.tasks.send_daily_test_report',
+        'args': (),
+        'options': {},
+        'schedule': crontab(day_of_week=1, minute='*/1')
+    }
+}
